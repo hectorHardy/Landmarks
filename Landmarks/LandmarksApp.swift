4b8a3a726d115d8caa8c3aef6b62b8904c6c305f
Hector Hardy <hectorhardy02@icloud.com>
2025-09-30 12:19:47 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hector Hardy on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
